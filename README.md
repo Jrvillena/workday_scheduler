17f057032aaa7b27a674abfccd06cdb898289092
justin <justinrvillena@gmail.com>
2021-04-15 13:44:31 -0700

    variables for cintainers created

--- README.md
+++ README.md
@@ -4,4 +4,6 @@ created container for time 8am block<br>
 added row amount to container<br>
 added bootstrap grid option to container<br>
 copied container amount to match office hour amounts<br>
-
+created let variables per container<br>
+created ready function<br>
+created string for date layout<br>
--- script.js
+++ script.js
@@ -9,3 +9,29 @@ let workDay = {
     "3 PM": "",
     "4 PM": "",
   };
+  $(document).ready(function(){
+    if(!localStorage.getItem('workDay')) {
+      updateCalendarTasks(workDay);
+    } else {
+      updateCalendarTasks(JSON.parse(localStorage.getItem('workDay')));
+    }
+  })
+  $('#date-today h6').text(moment().format('dddd') + ", " + moment().format('MMMM Do YYYY, h:mm:ss a'));
+
+  let counter = 1;
+for(const property in workDay) {
+  let textEntry = "#text-entry" + counter;
+  $(textEntry).text(workDay[property]);
+  let timeId = "#time" + counter;
+  let presentHour = moment().hour();
+  let timeString = $(timeId).text();
+  let timeNumber = hourNumberFromHourString(timeString);  
+  if(timeNumber < presentHour) {
+    $(textEntry).addClass("past-hour");
+  } else if (timeNumber > presentHour) {
+    $(textEntry).addClass("future-hour");
+  } else {
+    $(textEntry).addClass("present-hour");
+  }
+  counter ++;
+}
